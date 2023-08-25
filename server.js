fe35622ea4d25dbd1dbaaa5c6479f7e960c252e1
Md. Abir mahmud <abirmahmud5665@gmail.com>
2023-08-25 19:23:12 +0600

    created message sending api

---
+++ server.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const messageCollection = client.db("pathnaDB").collection("messages");
+
+    app.post("/messages", async (req, res) => {
+      const messages = req.body;
+      const result = await messageCollection.insertOne(messages);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//default
+app.get("/", (req, res) => {
+  res.status(200).send(["Server is running"]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
